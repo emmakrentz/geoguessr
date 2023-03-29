52fb3ab66b0ea42c9501f7abdbea6c13bd12909a
em <92812849+emmakrentz@users.noreply.github.com>
2023-03-29 13:32:13 -0700

    Update datapull.py

--- datapull.py
+++ datapull.py
@@ -1,3 +1,9 @@
+# function to parse metadata from a geoguessr game and automatically label game screenshots with their latitude-longitude coordinates 
+# process: play game of geoguessr, manually screenshot each location, copy game url, run script
+# each image is cropped, resized, labelled with its coordinates and country codes, moved to cleaned data folder for analysis
+
+
+# import packages
 from bs4 import BeautifulSoup
 import requests
 import os
@@ -9,6 +15,7 @@ def geog_location(url):
     lat_long = []
     page = requests.get(url)
     soup = BeautifulSoup(page.content,'html.parser')
+    
     # provides info on coordinates for each round
     for x in eval(str(soup).split('"rounds":[')[1].split(']')[0]):
         lat_long.append(str(x['lat'])+','+str(x['lng'])+','+x['streakLocationCode'])
@@ -30,9 +37,9 @@ def geog_location(url):
         old_file = screenshot_list[y]
         new_file = lat_long[y]+'.png'
         im = Image.open(old_path+old_file)
-        imCrop = im.crop((0, 272, 2880, 1626)) #corrected
+        imCrop = im.crop((0, 272, 2880, 1626)) # removes header and taskbar, leaving only image
         imCrop.save(new_path + new_file, "PNG", quality=100)
-        os.remove(old_path+old_file)
+        os.remove(old_path+old_file) # remove files from screenshots folder
     # create new file folder with images at 2% and 4% the size for analysis
         imCrop.resize((58,27)).save(resized_path1 + new_file, "PNG")
         imCrop.resize((115,54)).save(resized_path2 + new_file, "PNG")
